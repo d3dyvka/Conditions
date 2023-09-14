9a2ada7defe1d004ca38d79b4b6c8af9dbe15535
Rozhkov Ivan <vanroz333@gmail.com>
2023-09-14 18:31:39 +0700

    first tusk

---
+++ 1_задание.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if a > b:
+    if a > c:
+        print("Наибольшее число :", a)
+if b > a:
+    if b > c:
+        print("Наибольшее число :", b)
+if c > a:
+    if c > b:
+        print("Наибольшее число :", c)
